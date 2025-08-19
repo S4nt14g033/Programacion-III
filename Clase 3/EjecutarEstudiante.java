d07e2bb552d373bd7f4006910a3a86e31c81f0a6
S4nt14g033 <nsnino@estudiante.uniajc.edu.co>
2025-08-19 17:39:13 +0000

    cracion del arreglo estudiantes

--- Clase 3/EjecutarEstudiante.java
+++ Clase 3/EjecutarEstudiante.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class EjecutarEstudiante {
 
     public static void main(String[] args) {
@@ -10,5 +12,16 @@ public class EjecutarEstudiante {
         System.out.println(objEst1);
         System.out.println(objEst2);
         System.out.println(objEst3);
+
+        //creacion arreglo de objetos (Estudiantes)
+        
+        Estudiante[] e = new Estudiante[4];
+
+        e[0] = objEst1;
+        e[1] = objEst2;
+        e[2] = objEst3;
+        e[3] = new Estudiante(4, 100894094, "Peter", "ingenieria");
+
+        System.out.println(Arrays.toString(e));
     }
 }
