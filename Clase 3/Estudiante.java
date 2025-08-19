54eb4d8a9142a22509312e4198cf34ee97fcd827
S4nt14g033 <nsnino@estudiante.uniajc.edu.co>
2025-08-19 17:02:41 +0000

    guardar clase 3

---
+++ Clase 3/Estudiante.java
@@ -0,0 +1,22 @@
+Public class Estudiante {
+    //atributos
+    private int id;
+    private int cedula;
+    private String nombre;
+    private String facultad;
+
+    //constructor
+    public Estudiante(int id, int cedula, String nombre, String facultad) {
+        //autoreferencia para no crear otra variable
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.facultad = facultad;
+
+    }
+
+    //Metodo toString
+    public String toString() {
+        return "Estudiante: [id: " + id + "cedula: " + cedula + "Nombre: " + nombre + "facultad: " + facultad + " ] ";
+    }
+}
