027e2d5525146f713939880ee71336ed868708a2
S4nt14g033 <nsnino@estudiante.uniajc.edu.co>
2025-09-30 16:53:42 +0000

    clase 8

--- Clase 8/Tel.java
+++ Clase 8/Tel.java
@@ -33,9 +33,11 @@ public class Tel {
         int[] ejemplo1 = {1,2,3,4,5,6,7,8,9,0};
         int[] ejemplo2 = {3,0,0,1,4,5,6,7};
         int[] ejemplo3 = {3,0,0,6,2,1,9,5,3,3};
+        int[] ejemplo4 = {3,5,4,6,7,8,9,4,5,6,4,6};
 
         System.out.println(formatoTelefono(ejemplo1));
         System.out.println(formatoTelefono(ejemplo2));
         System.out.println(formatoTelefono(ejemplo3));
+        System.out.println(formatoTelefono(ejemplo4));
     }
 }
